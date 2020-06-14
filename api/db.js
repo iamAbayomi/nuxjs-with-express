d36b6e8e8589d7b11184c61ed4119a54898e8e33
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-14 23:41:05 +0100

    Inital Commit

---
+++ api/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+//mongodb database connection string. change it as per needed
+
+mongoose.connect('mongodb://localhost/mydb',{
+                            useNewUrlParser: true,
+                            useUnifiedTopology: true,
+                            useFindAndModify: false,
+                            useCreateIndex: true
+                                        });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once( 'open', function callback(){
+    console.log('MongoDB Connected');
+})
+
+module.exports = db
